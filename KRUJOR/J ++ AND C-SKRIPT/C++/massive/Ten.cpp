ff0a9ff8849949b67dfd26a7a0a478fad60487f2
ARGBX <student1619@alm.hexly28>
2025-02-24 13:32:11 +0500

    USA

---
+++ KRUJOR/J ++ AND C-SKRIPT/C++/massive/Ten.cpp
@@ -0,0 +1,5 @@
+//
+// Created by student1619 on 20.02.25.
+//
+
+#include "Ten.h"
---
+++ KRUJOR/J ++ AND C-SKRIPT/C++/massive/Ten.h
@@ -0,0 +1,16 @@
+//
+// Created by student1619 on 20.02.25.
+//
+
+#ifndef TEN_H
+#define TEN_H
+
+
+
+class Ten {
+
+};
+
+
+
+#endif //TEN_H
