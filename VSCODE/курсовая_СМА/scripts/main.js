261c8bf4a9849629737115056cd10f8fcf4dbda7
ZNAKLATINSKIY <maxim.sokolov2077@mail.ru>
2025-03-18 23:05:56 +0600

    адаптив ебаный

---
+++ VSCODE/курсовая_СМА/scripts/main.js
@@ -0,0 +1,24 @@
+// Копирование токена
+function CopyFunction() {
+    const copyText = document.getElementById("tokenTextCopy");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(copyText.value).then ( function() {
+        showCustomAlert("Токен скопирован.");
+    } ).catch(function(err) {
+        showCustomAlert("Ошибка при копировании: " + err);
+    });
+}
+// Окно о копировании
+function showCustomAlert(message) {
+    const alertBox = document.getElementById("customAlert");
+    alertBox.textContent = message;  // Устанавливаем текст уведомления
+    alertBox.style.display = "block"; // Показываем уведомление
+
+    // Прячем окно через 3 секунды
+    setTimeout(function() {
+        alertBox.style.display = "none";},
+        3000);
+}
+const tokenCopyButton = document.getElementById("tokenCopy");
+tokenCopyButton.onclick = CopyFunction;
