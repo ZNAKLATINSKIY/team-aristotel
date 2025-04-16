680646eb2fc19a6bf625528d75446f2de8ffb5f9
ZNAKLATINSKIY <maxim.sokolov2077@mail.ru>
2025-04-16 22:20:48 +0600

    16.04.2025

--- VSCODE/курсовая_СМА/scripts/animations/main_animations.js
+++ VSCODE/курсовая_СМА/scripts/animations/main_animations.js
@@ -1,35 +1,35 @@
 import { animate, stagger, inView } from "https://cdn.jsdelivr.net/npm/motion@latest/+esm"
 
 // Заголовок хэдера
-inView(".header__title", (target) => {
+inView(".header__title", () => {
     animate('.header__title', {x: ['-700px', '0px'], opacity: ['0%', '100%']}, {
         duration: 1.2, delay: stagger(0.07)
     });
 });
 // Параграф хэдера
-inView(".header__parag", (target) => {
+inView(".header__parag", () => {
     animate('.header__parag', {x: ['-1000px', '0px'], opacity: ['0%', '100%']}, {
         duration: 1.7, delay: stagger(0.07)
     });
 });
 
 // Правая часть контента хэдера
-inView(".content__right", (target) => {
+inView(".content__right", () => {
     animate('.content__right-one', {x: ['700px', '0px'], opacity: ['0%', '100%']}, {
         duration: 1.2, delay: stagger(0.07)
     });
 });
-inView(".content__right", (target) => {
+inView(".content__right", () => {
     animate('.content__right-two', {x: ['700px', '0px'], opacity: ['0%', '100%']}, {
         duration: 1.7, delay: stagger(0.07)
     });
 });
-inView(".content__right", (target) => {
+inView(".content__right", () => {
     animate('.content__right-three', {x: ['700px', '0px'], opacity: ['0%', '100%']}, {
         duration: 2.2, delay: stagger(0.07)
     });
 });
-inView(".content__right-one", (target) => {
+inView(".content__right-one", () => {
     animate('.content__right-four', {x: ['700px', '0px'], opacity: ['0%', '100%']}, {
         duration: 2.7, delay: stagger(0.07)
     });
@@ -38,19 +38,14 @@ inView(".content__right-one", (target) => {
 
 
 // Кнопки регистрации и входа в нижней части хэдера
-inView(".button__login", (target) => {
+inView(".button__login", () => {
     animate('.button__login', {x: ['0px', '0px'], opacity: ['-12%', '100%']}, {
         duration: 1.7, delay: stagger(2)
     });
 });
-inView(".button__register", (target) => {
+inView(".button__register", () => {
     animate('.button__register', {x: ['0px', '0px'], opacity: ['0%', '100%']}, {
         duration: 2.2, delay: stagger(2)
     });
 });
 
-inView(".purshared__title", (target) => {
-    animate('.purshared__title', {x: ['-700px', '0px'], opacity: ['0%', '100%']}, {
-        duration: 1.2, delay: stagger(2)
-    });
-});
