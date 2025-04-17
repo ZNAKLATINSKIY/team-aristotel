f83331f6cb8bc0193c27039defcd561dbccfbd78
ZNAKLATINSKIY <maxim.sokolov2077@mail.ru>
2025-04-18 00:18:53 +0600

    18.04.2025

--- VSCODE/курсовая_СМА/scripts/faq_page/faq.js
+++ VSCODE/курсовая_СМА/scripts/faq_page/faq.js
@@ -37,209 +37,24 @@ function showGeneralFaq() {
 generalFaq.onclick = showGeneralFaq;
 accountFaq.onclick = showAccountFaq;
 
-// Кликабельность элементов FAQ
-const questions = document.getElementsByClassName("question");
-const answerSmall = document.getElementsByClassName("answer__small");
 
+// Кликабельность элементов FAQ
 // Функция для блоков с большим количеством текста
 function showActiveQuestion() {
-    if ((window.innerWidth >= 1440) && (window.innerWidth <= 1650)) {
-        if(this.style.minHeight === "170px") {
-            this.style.minHeight = "50px";
-            this.style.background = "rgba(61, 0, 18, 0)";
-            this.querySelector(".arrow").style.transform = "rotate(0deg)";
-        } else {
-            this.style.minHeight = "170px";
-            this.style.background = "rgba(61, 0, 18, 0.6)";
-            this.querySelector(".arrow").style.transform = "rotate(90deg)";
-        }
-    }
-    else if ((window.innerWidth >= 1200) && window.innerWidth <= 1440) {
-        if(this.style.minHeight === "180px") {
-            this.style.minHeight = "50px";
-            this.style.background = "rgba(61, 0, 18, 0)";
-            this.querySelector(".arrow").style.transform = "rotate(0deg)";
-        } else {
-            this.style.minHeight = "180px";
-            this.style.background = "rgba(61, 0, 18, 0.6)";
-            this.querySelector(".arrow").style.transform = "rotate(90deg)";
-        }
-    }
-    else if ((window.innerWidth >= 856) && (window.innerWidth <= 1199)) {
-        if(this.style.minHeight === "225px") {
-            this.style.minHeight = "50px";
-            this.style.background = "rgba(61, 0, 18, 0)";
-            this.querySelector(".arrow").style.transform = "rotate(0deg)";
-        } else {
-            this.style.minHeight = "225px";
-            this.style.background = "rgba(61, 0, 18, 0.6)";
-            this.querySelector(".arrow").style.transform = "rotate(90deg)";
-        }
-    }
-    else if ((window.innerWidth >= 811) && (window.innerWidth <= 855)) {
-        if(this.style.minHeight === "230px") {
-            this.style.minHeight = "50px";
-            this.style.background = "rgba(61, 0, 18, 0)";
-            this.querySelector(".arrow").style.transform = "rotate(0deg)";
-        } else {
-            this.style.minHeight = "230px";
-            this.style.background = "rgba(61, 0, 18, 0.6)";
-            this.querySelector(".arrow").style.transform = "rotate(90deg)";
-        }
-    }
-    else if ((window.innerWidth >= 650) && (window.innerWidth <= 810)) {
-        if(this.style.minHeight === "235px") {
-            this.style.minHeight = "50px";
-            this.style.background = "rgba(61, 0, 18, 0)";
-            this.querySelector(".arrow").style.transform = "rotate(0deg)";
-        } else {
-            this.style.minHeight = "235px";
-            this.style.background = "rgba(61, 0, 18, 0.6)";
-            this.querySelector(".arrow").style.transform = "rotate(90deg)";
-        }
-    }
-    else if ((window.innerWidth >= 450) && (window.innerWidth <= 649)) {
-        if(this.style.minHeight === "260px") {
-            this.style.minHeight = "50px";
-            this.style.background = "rgba(61, 0, 18, 0)";
-            this.querySelector(".arrow").style.transform = "rotate(0deg)";
-        } else {
-            this.style.minHeight = "260px";
-            this.style.background = "rgba(61, 0, 18, 0.6)";
-            this.querySelector(".arrow").style.transform = "rotate(90deg)";
-        }
-    }
-    else if ((window.innerWidth >= 430) && (window.innerWidth <= 449)) {
-        if(this.style.minHeight === "235px") {
-            this.style.minHeight = "50px";
-            this.style.background = "rgba(61, 0, 18, 0)";
-            this.querySelector(".arrow").style.transform = "rotate(0deg)";
-        } else {
-            this.style.minHeight = "235px";
-            this.style.background = "rgba(61, 0, 18, 0.6)";
-            this.querySelector(".arrow").style.transform = "rotate(90deg)";
-        }
-    }
-    else if ((window.innerWidth >= 375) && (window.innerWidth <= 429)) {
-        if(this.style.minHeight === "258px") {
-            this.style.minHeight = "50px";
-            this.style.background = "rgba(61, 0, 18, 0)";
-            this.querySelector(".arrow").style.transform = "rotate(0deg)";
-        } else {
-            this.style.minHeight = "258px";
-            this.style.background = "rgba(61, 0, 18, 0.6)";
-            this.querySelector(".arrow").style.transform = "rotate(90deg)";
-        }
+    if (this.querySelector('.question__answer').style.display === "block") {
+        this.style.background = "none";
+        this.querySelector('.question__answer').style.display = "none";
     }
     else {
-        if(this.style.minHeight === "150px") {
-            this.style.minHeight = "50px";
-            this.style.background = "rgba(61, 0, 18, 0)";
-            this.querySelector(".arrow").style.transform = "rotate(0deg)";
-        } else {
-            this.style.minHeight = "150px";
-            this.style.background = "rgba(61, 0, 18, 0.6)";
-            this.querySelector(".arrow").style.transform = "rotate(90deg)";
-        }
-    }
-}
-
-// Функция для клика на блоки с маленьким количеством текста
-function showActiveQuestionSmall() {
-    if ((window.innerWidth >= 1440) && (window.innerWidth <= 1650)) {
-        if(this.style.minHeight === "140px") {
-            this.style.minHeight = "50px";
-            this.style.background = "rgba(61, 0, 18, 0)";
-            this.querySelector(".arrow").style.transform = "rotate(0deg)";
-        } else {
-            this.style.minHeight = "140px";
-            this.style.background = "rgba(61, 0, 18, 0.6)";
-            this.querySelector(".arrow").style.transform = "rotate(90deg)";
-        }
-    }
-    else if ((window.innerWidth >= 856) && (window.innerWidth <= 1199)) {
-        if(this.style.minHeight === "160px") {
-            this.style.minHeight = "50px";
-            this.style.background = "rgba(61, 0, 18, 0)";
-            this.querySelector(".arrow").style.transform = "rotate(0deg)";
-        } else {
-            this.style.minHeight = "160px";
-            this.style.background = "rgba(61, 0, 18, 0.6)";
-            this.querySelector(".arrow").style.transform = "rotate(90deg)";
-        }
-    }
-    else if ((window.innerWidth >= 811) && (window.innerWidth <= 855)) {
-        if(this.style.minHeight === "170px") {
-            this.style.minHeight = "50px";
-            this.style.background = "rgba(61, 0, 18, 0)";
-            this.querySelector(".arrow").style.transform = "rotate(0deg)";
-        } else {
-            this.style.minHeight = "170px";
-            this.style.background = "rgba(61, 0, 18, 0.6)";
-            this.querySelector(".arrow").style.transform = "rotate(90deg)";
-        }
-    }
-    else if ((window.innerWidth >= 750) && (window.innerWidth <= 810)) {
-        if(this.style.minHeight === "180px") {
-            this.style.minHeight = "50px";
-            this.style.background = "rgba(61, 0, 18, 0)";
-            this.querySelector(".arrow").style.transform = "rotate(0deg)";
-        } else {
-            this.style.minHeight = "180px";
-            this.style.background = "rgba(61, 0, 18, 0.6)";
-            this.querySelector(".arrow").style.transform = "rotate(90deg)";
-        }
-    }
-    else if ((window.innerWidth >= 450) && (window.innerWidth <= 749)) {
-        if(this.style.minHeight === "165px") {
-            this.style.minHeight = "50px";
-            this.style.background = "rgba(61, 0, 18, 0)";
-            this.querySelector(".arrow").style.transform = "rotate(0deg)";
-        } else {
-            this.style.minHeight = "165px";
-            this.style.background = "rgba(61, 0, 18, 0.6)";
-            this.querySelector(".arrow").style.transform = "rotate(90deg)";
-        }
-    }
-    else if ((window.innerWidth >= 400) && (window.innerWidth <= 449)) {
-        if(this.style.minHeight === "155px") {
-            this.style.minHeight = "50px";
-            this.style.background = "rgba(61, 0, 18, 0)";
-            this.querySelector(".arrow").style.transform = "rotate(0deg)";
-        } else {
-            this.style.minHeight = "155px";
-            this.style.background = "rgba(61, 0, 18, 0.6)";
-            this.querySelector(".arrow").style.transform = "rotate(90deg)";
-        }
-    }
-    else if ((window.innerWidth >= 375) && (window.innerWidth <= 399)) {
-        if(this.style.minHeight === "165px") {
-            this.style.minHeight = "50px";
-            this.style.background = "rgba(61, 0, 18, 0)";
-            this.querySelector(".arrow").style.transform = "rotate(0deg)";
-        } else {
-            this.style.minHeight = "165px";
-            this.style.background = "rgba(61, 0, 18, 0.6)";
-            this.querySelector(".arrow").style.transform = "rotate(90deg)";
-        }
-    }
-    else {
-        if(this.style.minHeight === "150px") {
-            this.style.minHeight = "50px";
-            this.style.background = "rgba(61, 0, 18, 0)";
-            this.querySelector(".arrow").style.transform = "rotate(0deg)";
-        } else {
-            this.style.minHeight = "150px";
-            this.style.background = "rgba(61, 0, 18, 0.6)";
-            this.querySelector(".arrow").style.transform = "rotate(90deg)";
-        }
+        this.style.background = "#3d0011";
+        this.querySelector(".question__answer").style.display = "block";
     }
 }
 
 // Даём кнопкам возможность менять цвет и размер
+const questions = document.getElementsByClassName("question");
+
 for(let i = 0; i < questions.length; i++) {
     questions[i].onclick = showActiveQuestion;
 }
-for (let i = 0; i < answerSmall.length; i++) {
-    answerSmall[i].onclick = showActiveQuestionSmall;
-}
+
